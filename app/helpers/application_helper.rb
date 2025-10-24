2dc429b8033089e75c93a8f5ba2337e2f02d22ce
erina <erina.okabe55@gmail.com>
2025-10-24 18:43:46 +0900

    ログイン本番環境調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type.to_sym
+    when :notice, :success
+      "bg-green-100 text-green-800 border-green-400" 
+    when :alert, :error
+      "bg-red-100 text-red-800 border-red-400"
+    else
+      "bg-blue-100 text-blue-800 border-blue-400"
+    end
+  end
 end
