0c4b820ade5ef6707194028efd7a2bebf796caba
erina <erina.okabe55@gmail.com>
2025-11-24 04:49:05 +0900

    設定修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,18 +7,19 @@ bundle install
 
 # 2. Yarnロックファイルの削除と再インストール
 rm -f yarn.lock
-yarn install
 
-#  esbuildを明示的に実行 (rake経由の失敗回避) 
+# ▼▼▼ 修正--production=false を追加して、ビルドツールを確実にインストールさせる!! ▼▼▼
+yarn install --production=false
+
+# 3. ビルドコマンドの実行 (package.jsonの "build" を実行)
 yarn build
 
-# 3. アセットのプリコンパイル
-# (※すでに yarn build でJSは作られているので、ここではCSSなどの処理が中心)
+# 4. アセットのプリコンパイル
 bundle exec rake assets:precompile
 
-# 4. 古いアセットの削除
+# 5. 古いアセットの削除
 bundle exec rake assets:clean
 
-# 5. データベースのマイグレーション
-# 絶対reset,seedしないで！！！！！！！！！！！！！
+# 6. データベースのマイグレーション
+#resetしない！！！！！！！！！！！！！
 bundle exec rake db:migrate
