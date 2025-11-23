2679076f6b0c94de74ae7702d1c2f22d6447596d
erina <erina.okabe55@gmail.com>
2025-11-24 04:32:09 +0900

    Renderデプロイ用明示

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,16 +5,20 @@ set -o errexit
 # 1. Ruby Gemsのインストール
 bundle install
 
-# ▼▼▼ 修正ポイント：yarn.lock を削除して、その場で最適なものを入れ直させる ▼▼▼
+# 2. Yarnロックファイルの削除と再インストール
 rm -f yarn.lock
 yarn install
 
+#  esbuildを明示的に実行 (rake経由の失敗回避) 
+yarn build
+
 # 3. アセットのプリコンパイル
+# (※すでに yarn build でJSは作られているので、ここではCSSなどの処理が中心)
 bundle exec rake assets:precompile
 
 # 4. 古いアセットの削除
 bundle exec rake assets:clean
 
 # 5. データベースのマイグレーション
-#絶対reset,seedしないで！！！！！！！！！！！！！
+# 絶対reset,seedしないで！！！！！！！！！！！！！
 bundle exec rake db:migrate
