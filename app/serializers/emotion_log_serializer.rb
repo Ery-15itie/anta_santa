f1c201968adfe64cb4394083ef486f1944365d08
erina <erina.okabe55@gmail.com>
2025-11-24 05:56:41 +0900

    fix: 不要な serializer ファイルを削除してデプロイエラーを解消

--- app/serializers/emotion_log_serializer.rb
+++
@@ -1,9 +0,0 @@
-# EmotionLogオブジェクトをJSON形式に整形
-class EmotionLogSerializer
-  include JSONAPI::Serializer
-  
-  # 感情ログの属性を定義
-  attributes :id, :body, :emotion, :intensity, :created_at, :updated_at
-
-  # 関連ユーザー情報を含める必要がある場合、ここで belongs_to :user を定義
-end
