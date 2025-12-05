269d4eafffff1d9b584c17dc5957cbac235620a4
erina <erina.okabe55@gmail.com>
2025-12-06 03:56:23 +0900

    OGP修正

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -4,17 +4,11 @@ class ShareController < ApplicationController
   def show
     @ogp_image = OgpImage.find_by!(uuid: params[:id])
     
-    # ActiveStorageの画像URLを取得（絶対URLで）
-    if @ogp_image.image.attached?
-      @image_url = rails_blob_url(@ogp_image.image, only_path: false)
-    else
+    # 画像が添付されているか確認
+    unless @ogp_image.image.attached?
       render plain: 'Image Not Found', status: :not_found
       return
     end
-    
-    @title = "私の価値観 - サンタの書斎"
-    @description = "私の大切な価値観の星々を見つけました✨"
-    @share_url = request.original_url
     
     # レイアウトなしで表示
     render layout: false
